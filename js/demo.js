4dc6af659c8efddd0e9a15bf6bdcfd1449d5ee5c
pao3li7 <664278503@qq.com>
2016-09-08 17:20:21 +0800

    111111

--- js/demo.js
+++ js/demo.js
@@ -1,14 +1,19 @@
 var ny = 0;
 var rotYINT;
+var arrList = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14'];//下标列表
+    var oDiv = $('#thumbList').find('.thumb');
+        var arr = [];//另外14个红包列表
+        var indexsed='';
 $(function() {
     var oList = document.getElementById('list');
     var result;
-    var arrList = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14'];//下标列表
-    var arr = [];//另外14个红包列表
-    var oDiv = $('#thumbList').find('.thumb');
+
+    //根据分辨率控制文字的大小
+    $('body').css('font-size',window.screen.width/320*12)
+
     //获取15个红包金额列表并铺出
     $.ajax({
-        url: "list.json",
+        url: "json/list.json",
         async: true,
         dataType: "json",
         type: "post",
@@ -41,54 +46,71 @@ $(function() {
         'width': imgWidth * 1.5,
         height: imgHeight * 1.5
     });
+    //定位翻转后文字的位置
+    var newImgW=$('.thumb  img').width();
+     var newImgH=$('.thumb  img').height();
+    $('.imgContent').css({
+    	left:'1px',
+    	top:newImgH/2
+    });
+    //定位选中的文字
+    $('#aniDiv .imgContent').css({
+		left:newImgW/2,
+		top:newImgH/3*2
+    });
     //点击图片的操作
     $('.thumb.flip').click(function() {
 
         //执行了一次就取消方法							
         $('.thumb.flip').off();
-        var indexs = $(this).index();
+        indexsed = $(this).index();
         var _this=this;
         //获取结果ajax
             $.ajax({
-        url: "list.json",
+        url: "json/list.json",
         async: true,
         dataType: "json",
         type: "post",
         success: function(data) {
             if (data.rs == 1) {
             	//获取中奖结果
+            	var other=data.other;
+            	if(other==0){
+            		//没有剩余的红包，把链接指向支付界面
+            		$('.go').html('继续支付');
+            		$('.go').click(function(){
+            			window.location='http://www.baidu.com';
+            		});
+            		
+            	}else{
+            		//请求再次拆红包
+            	}
                 result = data.result;
                 $('#layers b,#aniDiv b').html(result);
-		        Alist(_this, $(this), result, 10, indexs);
-		        
+		        Alist(_this, $(this), result, 10, indexsed);
+		         $(_this).css('visibility', 'hidden');
 
             }
         }
     });
 
-
         //开始旋转其他的div					
-        $('.sure').click(function() {
-            $('#layers').hide();
-            clickedFn(indexs);
+        $('.sure,.go,#layers h3 span').click(function() {
+            $('#layers,.tips').hide();
         })
 
     });
-    //点击弹框隐藏
-    $('.go').click(function() {
-        $('#layers').hide();
-    })
+
+})
 
     //点击确定执行的全部翻转函数
     function clickedFn(indexs) {
         arrList.splice(indexs, 1);
         for (var i = 0; i < arrList.length; i++) {
-            var time = 500 * i;
-            Alist(oDiv, arrList[i], arr[i], time, null);
+            var time = 300 * i;
+            Alist(oDiv, arrList[i], arr[i], time, indexsed);
         }
     }
-
-})
 
 //点击的效果
 function Alist(obj, indexs, result, time, fitstI) {//对象，不是第一次下标，结果，定时器时间，第一次的下标
@@ -96,9 +118,17 @@ function Alist(obj, indexs, result, time, fitstI) {//对象，不是第一次下
         setTimeout(function() {
             var objs = obj.get(indexs);
             objs.querySelector('.thumb-wrapper').className = 'thumb-wrapper flipIt';
-            objs.querySelector('span').innerHTML = '<b>'+result+'</b>元';
+            objs.querySelector('b').innerHTML =result;
         },
         time);
+        if(indexsed==14 && indexs==13 || indexs==14){
+        	setTimeout(function(){
+        		$('.tips').height(document.body.scrollHeight).show();
+        	  	$('#layers').show();
+        },6000)
+        }
+        
+      
 
     } else {
         //点击第一个的效果
@@ -116,8 +146,8 @@ function Alist(obj, indexs, result, time, fitstI) {//对象，不是第一次下
                 left: getOff[0],
                 top: getOff[1]
             }).show();
-        }console.log(indexs);
-        indexs.css('visibility', 'hidden');
+        }
+       
         //放大再旋转；然后执行回调
         $('#aniDiv img').animate({
             width: imgW * 1.5,
@@ -141,6 +171,20 @@ function Alist(obj, indexs, result, time, fitstI) {//对象，不是第一次下
                         height: imgH + 10
                     },
                     500);
+                    	$('#aniDiv img').animate({
+                        width: imgW,
+                        height:imgH
+                    },
+                    500);
+                    	                    //动画选中文字
+                    var newImgW=$('.thumb  img').width();
+ 					var newImgH=$('.thumb  img').height();
+                    $('#aniDiv .imgContent').animate({
+                        left: '1px',
+                        top:newImgH/2
+                    },
+                    500);
+                    
                 },
                 1000)
 
@@ -151,13 +195,28 @@ function Alist(obj, indexs, result, time, fitstI) {//对象，不是第一次下
                         height: imgH + 10
                     },
                     500);
+                    $('#aniDiv img').animate({
+                        width: imgW,
+                        height:imgH
+                    },
+                    500);
+                    //动画选中文字
+                    var newImgW=$('.thumb  img').width();
+ 					var newImgH=$('.thumb  img').height();
+                    $('#aniDiv .imgContent').animate({
+                        left: '1px',
+                        top:newImgH/2
+                    },
+                    500);
                 },
                 1000)
 
             }
-
-            setTimeout(function() {
-                $('#layers').show();
+           
+            setTimeout(function() {                            
+                $('.thumb-detail>img').hide();
+                $('.thumb-detail>span').show();
+               clickedFn(indexsed);
             },
             2000)
         });
